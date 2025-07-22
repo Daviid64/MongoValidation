5bdba05749d2f32cc722227abe21c311823a3e6d
Daviid64 <david.oguihandy@gmail.com>
2025-07-22 11:15:13 +0200

    Validation

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
 import deleteUserCron from "./middlewares/deleteUser.js";
-import auditDependencies from './middlewares/auditDependencies.js'
+import auditDependencies from './middlewares/auditDependencies.js';
+import helmet from "helmet";
+import cors from "cors"
+
 dotenv.config();
 
 const app = express();
@@ -13,6 +16,8 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
 
 // Démarre le cron
 deleteUserCron.start();
